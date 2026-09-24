f90e2684008aeaf8579c4d241345cd37c4c31da8
nkb2208 <nkb22082006@gmail.com>
2026-09-24 15:22:47 +0700

    fix something

--- stepstyle-app/src/pages/Cart.jsx
+++ stepstyle-app/src/pages/Cart.jsx
@@ -2,7 +2,6 @@ import Footer from "../layouts/Footer";
 import NavBar from "../layouts/NavBar";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
 function Cart(){
     let [quantity, setQuantity] = useState(1);
 
